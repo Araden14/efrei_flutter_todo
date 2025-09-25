a5238b5ca48a2f8ca702a3353ca95311c5eb1c93
GuilhermeDias02 <guilherme.alex.dias@gmail.com>
2025-09-25 10:10:02 +0200

    todo repo

--- lib/repositories/Todo/todo.repository.dart
+++ lib/repositories/Todo/todo.repository.dart
@@ -2,32 +2,32 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_todo/models/Todo/todo.model.dart';
 
 class UserRepository {
-  final CollectionReference _users = FirebaseFirestore.instance.collection(
-    'users',
+  final CollectionReference _todos = FirebaseFirestore.instance.collection(
+    'todos',
   );
 
-  Future<void> addUser(TodoModel user) async {
-    await _users.doc(user.id).set(user.toJson());
+  Future<void> addTodo(TodoModel todo) async {
+    await _todos.doc(todo.id).set(todo.toJson());
   }
 
   Future<TodoModel?> getUser(String id) async {
-    final doc = await _users.doc(id).get();
+    final doc = await _todos.doc(id).get();
     if (!doc.exists) return null;
     return TodoModel.fromFirestore(doc);
   }
 
-  Stream<List<TodoModel>> getAllUsers() {
-    return _users.snapshots().map(
+  Stream<List<TodoModel>> getAllTodos() {
+    return _todos.snapshots().map(
       (snapshot) =>
           snapshot.docs.map((doc) => TodoModel.fromFirestore(doc)).toList(),
     );
   }
 
-  Future<void> updateUser(TodoModel user) async {
-    await _users.doc(user.id).update(user.toJson());
+  Future<void> updatetodo(TodoModel todo) async {
+    await _todos.doc(todo.id).update(todo.toJson());
   }
 
-  Future<void> deleteUser(String id) async {
-    await _users.doc(id).delete();
+  Future<void> deleteTodo(String id) async {
+    await _todos.doc(id).delete();
   }
 }
