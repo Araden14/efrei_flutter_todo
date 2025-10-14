6360e825f9d829abea091be5d1a734a5a2c66d72
Araden14 <arnaud-danselme@hotmail.com>
2025-10-14 09:58:15 +0200

    firebase deployment, replace deprecated method

--- lib/widgets/todo_list.dart
+++ lib/widgets/todo_list.dart
@@ -85,7 +85,7 @@ class _StickyNoteCard extends StatelessWidget {
         borderRadius: BorderRadius.circular(8),
         boxShadow: [
           BoxShadow(
-            color: Colors.black.withOpacity(0.1),
+            color: Colors.black.withValues(alpha : 0.1),
             blurRadius: 8,
             offset: const Offset(2, 4),
           ),
@@ -101,7 +101,7 @@ class _StickyNoteCard extends StatelessWidget {
             child: Container(
               height: 20,
               decoration: BoxDecoration(
-                color: Colors.white.withOpacity(0.3),
+                color: Colors.white.withValues(alpha : 0.3),
                 borderRadius: const BorderRadius.only(
                   topLeft: Radius.circular(8),
                   topRight: Radius.circular(8),
